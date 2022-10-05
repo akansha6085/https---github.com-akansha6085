cb7fec046983bcc82f3a37ef63a8ae31babf8044
akansha6085 <akansha6085@gmail.com>
2022-10-05 12:52:48 +0530

    Skills sorted

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { motion } from "framer-motion"
+import React from 'react';
+import { motion } from "framer-motion";
 
 type Props = {
     directionLeft?:boolean;
@@ -15,14 +15,15 @@ function Skill({ directionLeft}: Props) {
         }}
         transition={{duration:1}}
         whileInView={{opacity: 1, x:0}}
-        src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png"
-        className="rounded-full border border-gray-500 object-cover h-24 w-28 md:h-28 xl:w-40 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        src="https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png"
+        className="rounded-full border border-gray-500 h-24 w-24 md:h-28 xl:w-32 xl:h-32 inset-0 object-cover object-center filter group-hover:grayscale transition duration-300 ease-in-out"
         />
         <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
             <p className="text-3xl font-bold text-black opacity-100">100%</p>
         </div>
-        </div>
+        </div> 
+       
         </div>
         
   )
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,14 +10,14 @@ export default function Skills({}: Props) {
     initial={{opacity:0}}
     whileInView={{opacity:1}}
     transition={{duration:1.5}}
-    className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[700px] max-h-[700px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
          <h3
             className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
               skills
                 </h3>
 
-                <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for current profieciency</h3>
-        <div className="grid grid-cols-4 gap-5">
+                <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm md-20">Hover over a skill for current profieciency</h3>
+        <div className="mt-20 pt-20 grid grid-cols-4 gap-5">
           <Skill/>
           <Skill/>
           <Skill/>
@@ -28,6 +28,9 @@ export default function Skills({}: Props) {
           <Skill/>
           <Skill/>
           <Skill/>
+          <Skill/>
+          <Skill/>
+       
         </div>
         </motion.div>
   )
